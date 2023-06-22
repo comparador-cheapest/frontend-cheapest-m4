52b45cc89e066f3ca8e3ecbcd42b619bcfa05912
Giovanni Bedoya <giovedoya@gmail.com>
2023-06-22 20:34:35 +0200

    get all articles

---
+++ src/services/articleService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+class ArticleService {    
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/articles`,
+    });
+
+    this.api.interceptors.request.use((config) => {
+      const storedToken = localStorage.getItem("authToken");
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+      return config;
+    });
+  }
+  getArticles() {
+    return this.api
+      .get("/")
+      .then(({ data }) => data)
+      .catch((err) => console.error(err));
+  }
+}
+
+const articleService = new ArticleService();
+
+export default articleService;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import articleService from '../services/articleService';
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  const getArticles = async () => {
+    try {
+      const response = await articleService.getArticles();
+      setArticles(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Homesasas</h1>
+      <ul>
+        {articles.map((elem) => (
+          <li key={elem._id}>
+            <p>{elem.product.image}</p>
+            <p>Price: {elem.price}</p>
+            <p>Supermarket: {elem.supermarket.name}</p>
+            <p>Product: {elem.product.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
